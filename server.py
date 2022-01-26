aee191e7d61e4477db26db9f147d89b7e986131b
Maura Thompson <thomaura@gmail.com>
2022-01-26 15:35:50 -0500

    Add user search input form HTML page and additional search routes in server.py

--- server.py
+++ server.py
@@ -12,5 +12,27 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/map/search")
+def search_page():
+    """ Render search page. """
+
+    return render_template("search.html")
+
+
+@app.route("/search_address")
+def search_use_API():
+    """ Take user input from HTML form and send to 
+        NYC Geosearch API to get lat lng coordinates
+        to render Google Map of search result. """
+    
+    # request.args.get user input
+    # send user input to NYC Geosearch
+    # save lat lng coordinates of search to variable or render error message
+    # ? jsonify lat lng ?
+    # ? pass lat lng as variable to Jinja
+
+    return render_template("searched_map.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
