28eef31be03024e93f3a4e4f981b7b4d0e87f921
Maura Thompson <thomaura@gmail.com>
2022-01-25 23:19:40 -0500

    Add custom markers to maps

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,22 +1,32 @@
 'use strict';
 // alert('maps.js is connected!');
 
-let map;
 
 function initMap() {
-    // const initialCoords = {
-    //     lat: 52.511225, 
-    //     lng: 13.442983
-    // };
+    const initialCoords = {
+        lat: 52.511225, 
+        lng: 13.442983
+    };
 
     const map = new google.maps.Map(
         document.querySelector('#map'), {
-            center: {
-                lat: 52.511225, 
-                lng: 13.442983
-            },
+            center: initialCoords,
             zoom: 13
         });
+
+    // Create new Google Maps Marker instance and configure
+    // configuration must have "postion" and "map" values
+    const initialMarker = new google.maps.Marker({
+        position: initialCoords,
+        icon: {
+            url: '/static/img/map-marker.png',
+            scaledSize: {
+                width: 55,
+                height: 55
+            }
+        },
+        map: map
+    }); 
 
     document.querySelector('#geocode-address').addEventListener(
         'click', () => {
@@ -29,6 +39,20 @@ function initMap() {
                     const userLocation = results[0].geometry.location;
                     console.log(userLocation);
 
+                    // construct custom Marker
+                    const userLocationMarker = new google.maps.Marker({
+                        position: userLocation,
+                        icon: {
+                            url: '/static/img/map-marker.png',
+                            scaledSize: {
+                                width: 55,
+                                height: 55
+                            }
+                        },
+                        map: map
+                    });
+                    
+                    // reset map with new userLocation info
                     map.setCenter(userLocation);
                     map.setZoom(16);
                 }
